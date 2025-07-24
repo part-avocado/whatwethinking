6c7c1464a815bfbe3dd7eac2021bc300e2741ef6
part-avocado <89282211+part-avocado@users.noreply.github.com>
2025-07-24 11:58:05 -0400

    helpp

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import io, { Socket } from 'socket.io-client';
+import io from 'socket.io-client';
 import axios from 'axios';
 import styled from 'styled-components';
 import { Send, Brain, Clock, Users, Timer, TrendingUp, Hash } from 'lucide-react';
